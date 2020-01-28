1b0da965486e04455e00c35757fe13ff0b082b5a
Alejandro Celaya <alejandro@alejandrocelaya.com>
2020-01-28 18:00:42 +0100

    Updated ErrorLogger so that it only logs as errors responses with status 5xx

--- test/Logger/ErrorLoggerTest.php
+++ test/Logger/ErrorLoggerTest.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace ShlinkioTest\Shlink\Common\Logger;
 
 use Exception;
+use Laminas\Diactoros\Response;
+use Laminas\Diactoros\ServerRequest;
 use PHPUnit\Framework\TestCase;
 use Prophecy\Prophecy\ObjectProphecy;
 use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
 use Shlinkio\Shlink\Common\Logger\ErrorLogger;
 
 class ErrorLoggerTest extends TestCase
@@ -21,15 +24,31 @@ class ErrorLoggerTest extends TestCase
         $this->errorLogger = new ErrorLogger($this->logger->reveal());
     }
 
-    /** @test */
-    public function delegatesIntoInternalLoggerWhenInvoked(): void
+    /**
+     * @test
+     * @dataProvider provideStatusCodes
+     */
+    public function delegatesIntoInternalLoggerWhenInvoked(int $status, string $expectedLevel): void
     {
         $e = new Exception('Something wrong');
-        $error = $this->logger->error((string) $e)->will(function (): void {
-        });
+        $log = $this->logger->log($expectedLevel, (string) $e);
 
-        ($this->errorLogger)($e);
+        ($this->errorLogger)($e, new ServerRequest(), (new Response())->withStatus($status));
 
-        $error->shouldHaveBeenCalledOnce();
+        $log->shouldHaveBeenCalledOnce();
+    }
+
+    public function provideStatusCodes(): iterable
+    {
+        yield 'status 500' => [500, LogLevel::ERROR];
+        yield 'status 503' => [503, LogLevel::ERROR];
+        yield 'status 504' => [504, LogLevel::ERROR];
+        yield 'status 400' => [400, LogLevel::DEBUG];
+        yield 'status 401' => [401, LogLevel::DEBUG];
+        yield 'status 403' => [403, LogLevel::DEBUG];
+        yield 'status 404' => [404, LogLevel::DEBUG];
+        yield 'status 499' => [499, LogLevel::DEBUG];
+        yield 'status 200' => [200, LogLevel::DEBUG];
+        yield 'status 301' => [301, LogLevel::DEBUG];
     }
 }
