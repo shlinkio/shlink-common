5ab53f1ba587b21dc1aa3d7a4873ce7c047bf98f
Alejandro Celaya <alejandro@alejandrocelaya.com>
2020-01-28 12:44:12 +0100

    Created new methods for date and array inputs

--- src/Validation/InputFactoryTrait.php
+++ src/Validation/InputFactoryTrait.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace Shlinkio\Shlink\Common\Validation;
 
+use DateTime;
 use Laminas\Filter;
+use Laminas\InputFilter\ArrayInput;
 use Laminas\InputFilter\Input;
 use Laminas\Validator;
+
+use function Functional\map;
 
 trait InputFactoryTrait
 {
@@ -31,6 +35,28 @@ trait InputFactoryTrait
             Validator\NotEmpty::STRING,
         ]]));
 
+        return $input;
+    }
+
+    private function createDateInput(
+        string $name,
+        bool $required = true,
+        array $formats = [DateTime::ATOM, 'Y-m-d']
+    ): Input {
+        $input = $this->createInput($name, $required);
+        $input->getValidatorChain()->attach(new ExcludingValidatorChain(...map(
+            $formats,
+            fn (string $format) => new Validator\Date(['format' => $format]),
+        )));
+        return $input;
+    }
+
+    private function createArrayInput(string $name, bool $required = true): Input
+    {
+        $input = new ArrayInput($name);
+        $input->setRequired($required)
+              ->getFilterChain()->attach(new Filter\StripTags())
+                                ->attach(new Filter\StringTrim());
         return $input;
     }
 }
