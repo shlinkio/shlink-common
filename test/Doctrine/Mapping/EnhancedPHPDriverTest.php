d205fd736885b60d98218cdd4f5c49e8a0b935ea
Alejandro Celaya <alejandro@alejandrocelaya.com>
2020-01-29 09:23:04 +0100

    Created EnhancedPHPDriverTest

---
+++ test/Doctrine/Mapping/EnhancedPHPDriverTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ShlinkioTest\Shlink\Common\Doctrine\Mapping;
+
+use Doctrine\Persistence\Mapping\ClassMetadata;
+use Doctrine\Persistence\Mapping\Driver\FileLocator;
+use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Prophecy\Prophecy\ObjectProphecy;
+use Shlinkio\Shlink\Common\Doctrine\Mapping\EnhancedPHPDriver;
+
+class EnhancedPHPDriverTest extends TestCase
+{
+    private ObjectProphecy $loader;
+    private ObjectProphecy $meta;
+
+    public function setUp(): void
+    {
+        $this->loader = $this->prophesize(FileLocator::class);
+        $this->loader->findMappingFile(Argument::any())->willReturn(
+            __DIR__ . '/../../../test-resources/mapping/fake.mapping.php',
+        );
+        $this->meta = $this->prophesize(ClassMetadata::class);
+    }
+
+    /**
+     * @test
+     * @dataProvider provideFuncStyle
+     */
+    public function internalFunctionIsInvokedBasedOnFunctionalStyle(array $args, int $metaExpectedCalls): void
+    {
+        $metaMethod = $this->meta->getFieldNames();
+
+        $driver = new EnhancedPHPDriver($this->loader->reveal(), [], ...$args);
+        $driver->loadMetadataForClass('', $this->meta->reveal());
+
+        $metaMethod->shouldHaveBeenCalledTimes($metaExpectedCalls);
+    }
+
+    public function provideFuncStyle(): iterable
+    {
+        yield 'func style' => [[true], 1];
+        yield 'no func style' => [[false], 0];
+        yield 'default func style' => [[], 0];
+    }
+}
