0afeefeba717beb4485af629152e39fac44268cb
Alejandro Celaya <alejandro@alejandrocelaya.com>
2021-12-05 09:51:22 +0100

    Added support for openswoole

--- config/dependencies.config.php
+++ config/dependencies.config.php
@@ -8,6 +8,7 @@ use Laminas\ServiceManager\AbstractFactory\ConfigAbstractFactory;
 use Laminas\ServiceManager\Factory\InvokableFactory;
 use Psr\Log\LoggerInterface;
 use RKA\Middleware\IpAddress;
+use Shlinkio\Shlink\Config\Factory\SwooleInstalledFactory;
 use Symfony\Component\Filesystem\Filesystem;
 
 return [
@@ -17,7 +18,7 @@ return [
             Filesystem::class => InvokableFactory::class,
 
             Middleware\CloseDbConnectionMiddleware::class => ConfigAbstractFactory::class,
-            Middleware\ContentLengthMiddleware::class => InvokableFactory::class,
+            Middleware\ContentLengthMiddleware::class => ConfigAbstractFactory::class,
             IpAddress::class => Middleware\IpAddressMiddlewareFactory::class,
 
             Logger\ErrorLogger::class => ConfigAbstractFactory::class,
@@ -26,6 +27,7 @@ return [
 
     ConfigAbstractFactory::class => [
         Middleware\CloseDbConnectionMiddleware::class => ['em'],
+        Middleware\ContentLengthMiddleware::class => [SwooleInstalledFactory::SWOOLE_INSTALLED],
         Logger\ErrorLogger::class => [LoggerInterface::class],
     ],
 
