3c560d1c9b49ff1ab3970a1e2008d4b26db1c1c6
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-09-17 11:23:26 +0200

    Fixed error when checking jwtSecret

--- src/Exception/MercureException.php
+++ src/Exception/MercureException.php
@@ -10,7 +10,7 @@ class MercureException extends RuntimeException implements ExceptionInterface
 {
     public static function missingJwtSecret(): self
     {
-        return new self('You have to provide a secret key for the JWT generation, under mercure.jwt_secret');
+        return new self('You have to provide a non-empty secret key for the JWT generation, under mercure.jwt_secret');
     }
 
     public static function missingHubUrl(): self
