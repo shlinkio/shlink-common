ebbe5ea25f481418773a69287bf8b9b65970fb85
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-10-28 19:25:47 +0200

    Updated ContentLengthMiddlewareTest to use PHPUnit mocks intead of prophecy

--- test/Middleware/ContentLengthMiddlewareTest.php
+++ test/Middleware/ContentLengthMiddlewareTest.php
@@ -5,10 +5,8 @@ declare(strict_types=1);
 namespace ShlinkioTest\Shlink\Common\Middleware;
 
 use Laminas\Diactoros\ServerRequestFactory;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use Prophecy\Argument;
-use Prophecy\PhpUnit\ProphecyTrait;
-use Prophecy\Prophecy\ObjectProphecy;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\StreamInterface;
 use Psr\Http\Server\RequestHandlerInterface;
@@ -16,95 +14,77 @@ use Shlinkio\Shlink\Common\Middleware\ContentLengthMiddleware;
 
 class ContentLengthMiddlewareTest extends TestCase
 {
-    use ProphecyTrait;
-
     private ContentLengthMiddleware $middleware;
     private bool $isSwoole;
-    private ObjectProphecy $handler;
+    private MockObject & RequestHandlerInterface $handler;
 
     public function setUp(): void
     {
         $this->isSwoole = false;
         $this->middleware = new ContentLengthMiddleware(fn () => $this->isSwoole);
-        $this->handler = $this->prophesize(RequestHandlerInterface::class);
+        $this->handler = $this->createMock(RequestHandlerInterface::class);
     }
 
     /** @test */
     public function responseIsReturnedAsIsWhenIsSwoole(): void
     {
         $this->isSwoole = true;
-        $resp = $this->prophesize(ResponseInterface::class);
-        $respMock = $resp->reveal();
+        $respMock = $this->createMock(ResponseInterface::class);
+        $respMock->expects($this->never())->method('hasHeader')->with('Content-Length');
+        $respMock->expects($this->never())->method('getBody');
         $request = ServerRequestFactory::fromGlobals();
-        $handle = $this->handler->handle($request)->willReturn($respMock);
+        $this->handler->expects($this->once())->method('handle')->with($request)->willReturn($respMock);
 
-        $result = $this->middleware->process($request, $this->handler->reveal());
+        $result = $this->middleware->process($request, $this->handler);
 
         self::assertSame($respMock, $result);
-        $handle->shouldHaveBeenCalledOnce();
-        $resp->hasHeader('Content-Length')->shouldNotHaveBeenCalled();
-        $resp->getBody()->shouldNotHaveBeenCalled();
     }
 
     /** @test */
     public function responseIsReturnedAsIsWhenItAlreadyHasContentLength(): void
     {
-        $resp = $this->prophesize(ResponseInterface::class);
-        $hasHeader = $resp->hasHeader('Content-Length')->willReturn(true);
-        $respMock = $resp->reveal();
+        $respMock = $this->createMock(ResponseInterface::class);
+        $respMock->expects($this->once())->method('hasHeader')->with('Content-Length')->willReturn(true);
+        $respMock->expects($this->never())->method('getBody');
         $request = ServerRequestFactory::fromGlobals();
-        $handle = $this->handler->handle($request)->willReturn($respMock);
+        $this->handler->expects($this->once())->method('handle')->with($request)->willReturn($respMock);
 
-        $result = $this->middleware->process($request, $this->handler->reveal());
+        $result = $this->middleware->process($request, $this->handler);
 
         self::assertSame($respMock, $result);
-        $handle->shouldHaveBeenCalledOnce();
-        $hasHeader->shouldHaveBeenCalledOnce();
-        $resp->getBody()->shouldNotHaveBeenCalled();
     }
 
     /** @test */
     public function responseIsReturnedAsIsWhenBodySizeIsNull(): void
     {
-        $resp = $this->prophesize(ResponseInterface::class);
-        $hasHeader = $resp->hasHeader('Content-Length')->willReturn(false);
-        $body = $this->prophesize(StreamInterface::class);
-        $getSize = $body->getSize()->willReturn(null);
-        $getBody = $resp->getBody()->willReturn($body->reveal());
-        $respMock = $resp->reveal();
+        $respMock = $this->createMock(ResponseInterface::class);
+        $respMock->expects($this->once())->method('hasHeader')->with('Content-Length')->willReturn(false);
+        $respMock->expects($this->never())->method('withHeader');
+        $body = $this->createMock(StreamInterface::class);
+        $body->expects($this->once())->method('getSize')->willReturn(null);
+        $respMock->expects($this->once())->method('getBody')->willReturn($body);
         $request = ServerRequestFactory::fromGlobals();
-        $handle = $this->handler->handle($request)->willReturn($respMock);
+        $this->handler->expects($this->once())->method('handle')->with($request)->willReturn($respMock);
 
-        $result = $this->middleware->process($request, $this->handler->reveal());
+        $result = $this->middleware->process($request, $this->handler);
 
         self::assertSame($respMock, $result);
-        $handle->shouldHaveBeenCalledOnce();
-        $hasHeader->shouldHaveBeenCalledOnce();
-        $getSize->shouldHaveBeenCalledOnce();
-        $getBody->shouldHaveBeenCalledOnce();
-        $resp->withHeader(Argument::cetera())->shouldNotHaveBeenCalled();
     }
 
     /** @test */
     public function responseIsReturnedWithNewHeaderWhenBodySizeIsNotNull(): void
     {
-        $resp = $this->prophesize(ResponseInterface::class);
-        $hasHeader = $resp->hasHeader('Content-Length')->willReturn(false);
-        $body = $this->prophesize(StreamInterface::class);
-        $getSize = $body->getSize()->willReturn(100);
-        $getBody = $resp->getBody()->willReturn($body->reveal());
-        $respMock = $resp->reveal();
-        $withHeader = $resp->withHeader('Content-Length', '100')->willReturn($respMock);
+        $respMock = $this->createMock(ResponseInterface::class);
+        $respMock->expects($this->once())->method('hasHeader')->with('Content-Length')->willReturn(false);
+        $body = $this->createMock(StreamInterface::class);
+        $body->expects($this->once())->method('getSize')->willReturn(100);
+        $respMock->expects($this->once())->method('getBody')->willReturn($body);
+        $respMock->expects($this->once())->method('withHeader')->with('Content-Length', '100')->willReturn($respMock);
         $request = ServerRequestFactory::fromGlobals();
-        $handle = $this->handler->handle($request)->willReturn($respMock);
+        $this->handler->expects($this->once())->method('handle')->with($request)->willReturn($respMock);
 
-        $result = $this->middleware->process($request, $this->handler->reveal());
+        $result = $this->middleware->process($request, $this->handler);
 
         self::assertSame($respMock, $result);
-        $handle->shouldHaveBeenCalledOnce();
-        $hasHeader->shouldHaveBeenCalledOnce();
-        $getSize->shouldHaveBeenCalledOnce();
-        $getBody->shouldHaveBeenCalledOnce();
-        $withHeader->shouldHaveBeenCalledOnce();
     }
 }
