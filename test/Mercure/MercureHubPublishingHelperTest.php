c78dfbf632f17cf8c6b47b1fae4e433ba4b0cce3
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-10-28 19:14:21 +0200

    Updated MercureHubPublishingHelperTest to use PHPUnit mocks intead of prophecy

--- test/Mercure/MercureHubPublishingHelperTest.php
+++ test/Mercure/MercureHubPublishingHelperTest.php
@@ -4,9 +4,8 @@ declare(strict_types=1);
 
 namespace ShlinkioTest\Shlink\Common\Mercure;
 
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use Prophecy\PhpUnit\ProphecyTrait;
-use Prophecy\Prophecy\ObjectProphecy;
 use Shlinkio\Shlink\Common\Mercure\MercureHubPublishingHelper;
 use Shlinkio\Shlink\Common\UpdatePublishing\Update;
 use Symfony\Component\Mercure\HubInterface;
@@ -14,24 +13,21 @@ use Symfony\Component\Mercure\Update as MercureUpdate;
 
 class MercureHubPublishingHelperTest extends TestCase
 {
-    use ProphecyTrait;
-
     private MercureHubPublishingHelper $helper;
-    private ObjectProphecy $hub;
+    private MockObject & HubInterface $hub;
 
     public function setUp(): void
     {
-        $this->hub = $this->prophesize(HubInterface::class);
-        $this->helper = new MercureHubPublishingHelper($this->hub->reveal());
+        $this->hub = $this->createMock(HubInterface::class);
+        $this->helper = new MercureHubPublishingHelper($this->hub);
     }
 
     /** @test */
     public function updatesAreForwardedToMercure(): void
     {
-        $mercurePublish = $this->hub->publish(new MercureUpdate('foo', '{"bar":"baz"}'))->willReturn('');
-
+        $this->hub->expects($this->once())->method('publish')->with(
+            new MercureUpdate('foo', '{"bar":"baz"}'),
+        )->willReturn('');
         $this->helper->publishUpdate(Update::forTopicAndPayload('foo', ['bar' => 'baz']));
-
-        $mercurePublish->shouldHaveBeenCalledOnce();
     }
 }
