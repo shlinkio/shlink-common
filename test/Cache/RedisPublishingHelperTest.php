0c8ae0252fca1592a1993101e9b654144fe57e77
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-10-28 10:49:58 +0200

    Migrated RedisPublishingHelperTest to PHPUnit mocks

--- test/Cache/RedisPublishingHelperTest.php
+++ test/Cache/RedisPublishingHelperTest.php
@@ -4,30 +4,27 @@ declare(strict_types=1);
 
 namespace ShlinkioTest\Shlink\Common\Cache;
 
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Predis\ClientInterface;
-use Prophecy\PhpUnit\ProphecyTrait;
-use Prophecy\Prophecy\ObjectProphecy;
 use Shlinkio\Shlink\Common\Cache\RedisPublishingHelper;
 use Shlinkio\Shlink\Common\UpdatePublishing\Update;
 
 class RedisPublishingHelperTest extends TestCase
 {
-    use ProphecyTrait;
-
     private RedisPublishingHelper $helper;
-    private ObjectProphecy $predis;
+    private MockObject & ClientInterface $predis;
 
     public function setUp(): void
     {
-        $this->predis = $this->prophesize(ClientInterface::class);
-        $this->helper = new RedisPublishingHelper($this->predis->reveal());
+        $this->predis = $this->createMock(ClientInterface::class);
+        $this->helper = new RedisPublishingHelper($this->predis);
     }
 
     /** @test */
     public function publishingIsForwardedToRedisClient(): void
     {
+        $this->predis->expects($this->once())->method('__call')->with('publish', ['foo', '{"bar":"baz"}']);
         $this->helper->publishUpdate(Update::forTopicAndPayload('foo', ['bar' => 'baz']));
-        $this->predis->publish('foo', '{"bar":"baz"}')->shouldHaveBeenCalledOnce();
     }
 }
