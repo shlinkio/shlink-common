e2260d9c4328d366b0b7ff5e5bce24da893c8416
Alejandro Celaya <alejandrocelaya@gmail.com>
2026-05-10 08:57:16 +0200

    Apply array_unique inside TagsConverter

--- src/ObjectMapper/TagsConverter.php
+++ src/ObjectMapper/TagsConverter.php
@@ -8,11 +8,20 @@ use Attribute;
 use CuyZ\Valinor\Mapper\AsConverter;
 
 use function array_map;
+use function array_unique;
 use function mb_strtolower;
 use function preg_replace;
 use function strip_tags;
 use function trim;
 
+/**
+ * Converts a list of tags, applying:
+ * 1. Walk all entries and:
+ *   a) Strip tags and trim
+ *   b) Convert to lowercase
+ *   c) Replace spaces with dashes
+ * 2. Remove duplicates
+ */
 #[AsConverter]
 #[Attribute(Attribute::TARGET_PROPERTY | Attribute::TARGET_PARAMETER)]
 class TagsConverter
@@ -24,6 +33,8 @@ class TagsConverter
     public function map(array $value): array
     {
         // @phpstan-ignore return.type
-        return array_map(fn (string $tag) => preg_replace('/ /', '-', mb_strtolower(trim(strip_tags($tag)))), $value);
+        return array_unique(
+            array_map(fn (string $tag) => preg_replace('/ /', '-', mb_strtolower(trim(strip_tags($tag)))), $value),
+        );
     }
 }
